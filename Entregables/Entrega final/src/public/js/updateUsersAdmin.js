0ef569658d5cf226b59ca97c35f431f1e5eb07c4
Pablo Fernandez <pfernandez@fx2.com.uy>
2023-07-05 11:08:24 -0300

    arreglos

--- Entregables/Entrega final/src/public/js/updateUsersAdmin.js
+++ Entregables/Entrega final/src/public/js/updateUsersAdmin.js
@@ -3,46 +3,43 @@ const saveButton = document.getElementById("saveUsers");
 // Obtener todas las celdas td de la tabla
 const trElements = document.querySelectorAll('table tbody tr');
 // Array para almacenar los objetos de usuario
-const users = [];
+const usersToUpdate = { users: [] };
 
 // Recorrer cada celda td y construir el objeto de usuario
 trElements.forEach((tr) => {
-  const email = tr.querySelector('td:nth-child(1)').textContent.trim();
-  const name = tr.querySelector('td:nth-child(2)').textContent.trim();
-  const rol = tr.querySelector('td:nth-child(3) select').value;
+    const email = tr.querySelector('td:nth-child(1)').textContent.trim();
+    const name = tr.querySelector('td:nth-child(2)').textContent.trim();
+    const rol = tr.querySelector('td:nth-child(3) select').value;
 
 
-  users.push({
-    email: email,
-    name: name,
-    rol: rol
-  });
+    usersToUpdate.users.push({
+        email: email,
+        name: name,
+        rol: rol
+    });
 });
 
 const url = 'http://localhost:8080/config'
 const headers = {
-  'Content-Type': 'application/json'
+    'Content-Type': 'application/json'
 }
 const method = 'PATCH'
-const body = JSON.stringify(users)
-
-console.log(body)
+const body = JSON.stringify(usersToUpdate)
 
 saveButton.addEventListener("click", (event) => {
     fetch(url, {
         headers,
         method,
         body
-      })
-      .then(response => {
-        return response.json()
-      })
-      .then(data => {
-        Swal.fire("Done", "users were updated!", "success")
-      })
-      .catch(error => {
-        alert('error')
-        console.log(error)
-      })
     })
-    
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
+            Swal.fire("Done", "users were updated!", "success")
+        })
+        .catch(error => {
+            alert('error')
+            console.log(error)
+        })
+})
